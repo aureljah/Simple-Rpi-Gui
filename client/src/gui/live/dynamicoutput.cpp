d745e3c1910ae57d492c2355f4606c158b605eba
Aureljah <aolibe@gmail.com>
2021-02-14 23:58:30 +0100

    hotfix

--- client/src/gui/live/dynamicoutput.cpp
+++ client/src/gui/live/dynamicoutput.cpp
@@ -17,11 +17,6 @@ dynamicOutput::dynamicOutput(int pin, QString name, QWidget *parent) :
     ui->pwm_spinBox->setSingleStep(1);
     ui->pwm_slider->setRange(0, 100);
 
-    connect(ui->pwm_slider, &QSlider::valueChanged,
-            ui->pwm_spinBox, &QSpinBox::setValue);
-    connect(ui->pwm_spinBox, QOverload<int>::of(&QSpinBox::valueChanged),
-            ui->pwm_slider, &QSlider::setValue);
-
     this->updatePinValue(0);
 }
 
@@ -89,6 +84,7 @@ void dynamicOutput::updateLcdNumber()
 
 void dynamicOutput::updatePinValue(int new_value)
 {
+    //qInfo() << "updatePinValue value: " << value << "\n";
     if (new_value < 0)
         this->value = 0;
     else if (new_value > 100)
@@ -96,8 +92,14 @@ void dynamicOutput::updatePinValue(int new_value)
     else
         this->value = new_value;
 
+    ui->pwm_slider->blockSignals(true);
     ui->pwm_slider->setValue(this->value);
+    ui->pwm_slider->blockSignals(false);
+
+    ui->pwm_spinBox->blockSignals(true);
     ui->pwm_spinBox->setValue(this->value);
+    ui->pwm_spinBox->blockSignals(false);
+
     this->updateLcdNumber();
 
     if (this->value == 0)
@@ -137,12 +139,10 @@ void dynamicOutput::on_offButton_clicked(bool checked)
 
 void dynamicOutput::on_pwm_slider_valueChanged(int value)
 {
-    //qInfo() << "on_pwm_slider_valueChanged value: " << value << "\n";
     this->updatePinValue(value);
 }
 
 void dynamicOutput::on_pwm_spinBox_valueChanged(int arg1)
 {
-    //qInfo() << "on_pwm_spinBox_valueChanged value: " << arg1 << "\n";
     this->updatePinValue(arg1);
 }
